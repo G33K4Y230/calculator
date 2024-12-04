7d03a3d12aa2ccf6c0687cbcf9345f84419750c9
G33K4Y230 <gkandandji@gmail.com>
2024-12-04 13:49:17 +0000

    button definitions

--- buttons.js
+++ buttons.js
@@ -5,5 +5,145 @@ const buttons = [
         colStart: 1,
         rowEnd: 4,
         colEnd: 3
+    },
+    {
+        text: "M+",
+        rowStart: 3,
+        colStart: 3,
+        rowEnd: 4,
+        colEnd: 3
+    }, 
+    {
+        text: "MR",
+        rowStart: 3,
+        colStart: 4,
+        rowEnd: 4,
+        colEnd: 4
+    }, 
+    {
+        text: "<",
+        rowStart: 3,
+        colStart: 5,
+        rowEnd: 4,
+        colEnd: 6
+    },
+    {
+        text: "/",
+        rowStart: 4,
+        colStart: 4,
+        rowEnd: 5,
+        colEnd: 4
+    },
+    {
+        text: "*",
+        rowStart: 5,
+        colStart: 4,
+        rowEnd: 6,
+        colEnd: 5 
+    },
+    {
+        text: "-",
+        rowStart: 6,
+        colStart: 4,
+        rowEnd: 7,
+        colEnd: 5
+    },
+    {
+        text: "+",
+        rowStart: 7,
+        colStart: 4,
+        rowEnd: 8,
+        colEnd: 5
+    },
+    {
+        text: "+/-",
+        rowStart: 4,
+        colStart: 5,
+        rowEnd: 6,
+        colEnd: 6
+    },
+    {
+        text: "=",
+        rowStart: 6,
+        colStart: 5,
+        rowEnd: 8,
+        colEnd: 6
+    },
+    {
+        text: "9",
+        rowStart: 4,
+        colStart: 1,
+        rowEnd: 5,
+        colEnd: 2
+    },
+    {
+        text: "8",
+        rowStart: 4,
+        colStart: 2,
+        rowEnd: 5,
+        colEnd: 3
+    },
+    {
+        text: "7",
+        rowStart: 4,
+        colStart: 3, 
+        rowEnd: 5,
+        colEnd: 4
+    },
+    {
+        text: "6",
+        rowStart: 5,
+        colStart: 1,
+        rowEnd: 6,
+        colEnd: 2
+    },
+    {
+        text: "5",
+        rowStart: 5,
+        colStart: 2,
+        rowEnd: 6,
+        colEnd: 3
+    },
+    {
+        text: "4",
+        rowStart: 5,
+        colStart: 3,
+        rowEnd: 6,
+        colEnd: 4
+    },
+    {
+        text: "3",
+        rowStart: 6,
+        colStart: 1,
+        rowEnd: 7,
+        colEnd: 2
+    }, 
+    {
+        text: "2",
+        rowStart: 6,
+        colStart: 2,
+        rowEnd: 7,
+        colEnd: 3
+    },
+    {
+        text: "1",
+        rowStart: 6,
+        colStart: 3,
+        rowEnd: 7,
+        colEnd: 4
+    },
+    {
+        text: "0",
+        rowStart: 7,
+        colStart: 1,
+        rowEnd: 8,
+        colEnd: 3
+    },
+    {
+        text: ".",
+        rowStart: 7,
+        colStart: 3,
+        rowEnd: 8,
+        colEnd: 4
     }
 ]
