f1fce4a198f9f201cec7671d2d07cada210f793c
G33K4Y230 <gkandandji@gmail.com>
2024-12-04 16:00:26 +0000

    output styling

--- calculator.js
+++ calculator.js
@@ -3,7 +3,7 @@ const elementClassStyleButton = "bg-gradient-to-b from-white to-zinc-300 shadow-
                               + "hover:from-zinc-100 hover:to-zinc-400" + " "
                               + "active:from-zinc-300 active:to-white active:shadow-black/50 active:shadow-inner";
 const elementClassStyleOutput = "bg-gradient-to-b from-zinc-200 to-white shadow-inner shadow-black/50 border-black border-solid border-2" + " "
-                              + "flex flex-col";
+                              + "flex flex-col divide-y divide-black divide-y-1";
 
 /* SIMILAR PARAMETERS TO CSS grid-area */
 const generateElement2 = (rowStart, colStart, rowEnd, colEnd, text = "", id = "") => {
